fbd2d5f2b3edf346652d67f39edaa83d4658cc0f
nsaedae@gmail.com <nsaedae@gmail.com>
2020-12-29 17:39:03 +0900

    2020/12/29 네이버 실검 수집완료

--- Ch2/2_7_naver.py
+++ Ch2/2_7_naver.py
@@ -3,13 +3,19 @@
 이름 : 김철학
 내용 : 네이버 실시간 검색어 수집하기
 """
+import os
 import requests as req
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from datetime import datetime
 
-# 크롬 가상 웹브라우저 실행
-browser = webdriver.Chrome('./chromedriver.exe')
+# 리눅스용 Chrome 브라우저 설치
+# 크롬 가상 웹브라우저 실행(headless 모드)
+chrome_option = webdriver.ChromeOptions()
+chrome_option.add_argument('--headless')
+chrome_option.add_argument('--no-sandbox')
+chrome_option.add_argument('--disable-dev-shm-usage')
+browser = webdriver.Chrome('./chromedriver.exe', options=chrome_option)
 browser.implicitly_wait(3)
 
 # 네이버 데이터랩 이동
@@ -20,10 +26,14 @@ browser.implicitly_wait(3)
 item_boxs = browser.find_elements_by_css_selector('#content .selection_area .field_list ul:nth-child(1) > li > .item_box')
 
 # 디렉터리 생성
+dir = "./naver/{:%Y-%m-%d}".format(datetime.now())
+
+if not os.path.exists(dir):
+    os.makedirs(dir)
 
 # 파일 저장
 fname = "{:%y-%m-%d-%H-%M.txt}".format(datetime.now())
-file = open('./naver/'+fname, mode='w', encoding='utf8')
+file = open(dir+'/'+fname, mode='w', encoding='utf8')
 file.write('순위,제목,날짜\n')
 
 for item_box in item_boxs:
@@ -31,8 +41,10 @@ for item_box in item_boxs:
     file.write('%s,' % item_box.find_element_by_css_selector('.item_title').text)
     file.write('%s\n' % "{:%y%m%d%H%M%S}".format(datetime.now()))
 
-
 file.close()
+
+# 브라우저 종료
+browser.close()
 
 print('수집완료...')
 
