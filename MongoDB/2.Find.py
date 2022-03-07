e1b5c5d7a8261ed9d7fe117d8145b7431f22cb46
unknown <nsaedae@gmail.com>
2022-03-07 17:20:55 +0900

    2022/03/07 수업예제

---
+++ MongoDB/2.Find.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient as mongo
+from datetime import datetime
+
+# 1단계 - MongoDB 접속
+conn = mongo('mongodb://chhak:1234@192.168.56.101:27017')
+
+# 2단계 - DB 선택
+db = conn.get_database('test')
+
+# 3단계 - Collection 선택
+collection = db.get_collection('Member')
+
+# 4단계 - 쿼리실행
+rs = collection.find()
+
+for row in rs:
+    print('{}, {}, {}, {}, {}, {}'.format(row['uid'], row['name'], row['hp'], row['pos'], row['dep'], row['rdate']))
+
+# 5단계 - MongoDB 종료
+conn.close()
+
+
+
+
